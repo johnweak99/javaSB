65283f4c89ccd553142e561dd05b17e732eeb718
Johnweak99 <johnleeang@outlook.com>
2023-05-06 11:12:13 +0800

    first commit

---
+++ src/main/java/com/example/azurespring/AzureSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.azurespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/azurespring/AzureSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azurespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
